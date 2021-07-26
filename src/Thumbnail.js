39705aff493dedc29af9dda90894954431a13931
Bianca Afonso <bianca.afonso@wunderman.com>
2021-07-26 15:35:52 +0200

    Updates

--- src/Thumbnail.js
+++
@@ -1,9 +0,0 @@
-<div className="project">
-  <a href="./experiments">
-    <div className="project-image">
-      <img src="./experiments-image" alt="Project Image"/>
-    </div>
-    <div className="project-title">Experiments</div>
-    <div className="project-category">Personal</div>
-  </a>
-</div>
